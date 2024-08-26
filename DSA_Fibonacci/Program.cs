bc99adb57e9aadee64494e39c5c9f656ea28449c
Arlinda C Ibezim <ArlCIbe@outlook.com>
2024-08-26 14:01:39 -0500

    completed loop part

--- DSA_Fibonacci/Program.cs
+++ DSA_Fibonacci/Program.cs
@@ -1,13 +1,33 @@
-﻿namespace DSA_Fibonacci;
-class Program
-{
-    static void Main(string[] args)
-    {
+﻿using System.Security.Cryptography.X509Certificates;
 
+namespace DSA_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FibSeq();
+        }
+
+        public static void FibSeq()
+        {
+            int prevFib1 = 0;
+            int prevFib2 = 1;
+
+            for(int i = 0; i < 18; i++)
+            {
+                int newFib = prevFib1 + prevFib2;
+                Console.WriteLine($"{newFib}");
+                prevFib1 = prevFib2;
+                prevFib2 = newFib;
+                newFib = prevFib1 + prevFib2;
+            }
+        }
     }
 }
-/*Two variables to hold the previous two Fibonacci numbers
-A for loop that runs 18 times
-Create new Fibonacci numbers by adding the two previous ones
-Print the new Fibonacci number
-Update the variables that hold the previous two fibonacci numbers*/
+/*
+- Two variables to hold the previous two Fibonacci numbers
+- A for loop that runs 18 times
+- Create new Fibonacci numbers by adding the two previous ones
+- Print the new Fibonacci number
+- Update the variables that hold the previous two fibonacci numbers*/
