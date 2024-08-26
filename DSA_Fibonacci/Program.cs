d403b909f04d8eb1f50c43c57054026773db2dec
Arlinda C Ibezim <ArlCIbe@outlook.com>
2024-08-26 11:36:05 -0500

    started over

--- DSA_Fibonacci/Program.cs
+++ DSA_Fibonacci/Program.cs
@@ -1,5 +1,13 @@
-﻿class Program
+﻿namespace DSA_Fibonacci;
+class Program
 {
-    
+    static void Main(string[] args)
+    {
+
+    }
 }
-Console.WriteLine("Hello, World!");
+/*Two variables to hold the previous two Fibonacci numbers
+A for loop that runs 18 times
+Create new Fibonacci numbers by adding the two previous ones
+Print the new Fibonacci number
+Update the variables that hold the previous two fibonacci numbers*/
